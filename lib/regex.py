92c26247de509e1a75d30b979a85af34ebeb0f62
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-15 20:19:02 -0400

    complete lab

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,11 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"[A-Z][^0-9][\w|\W|0-9]{3}[^\s][\w|\W|0-9]{1,}"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"[0-9|\W]{4}[^\-][0-9|\W]{5,}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"(?=[^.]*\.)(?=[^$]*@[^$]*$)(?=[^@]*@[^@]*$)[^0-9][\w|\W|0-9]{1,}"
 email_regex = re.compile(email_address)
